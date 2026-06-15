fa6d5cd4bb23d50e89120951705f7d69f386663a
keritikin <keritikin@gmail.com>
2026-06-16 03:44:45 +0700

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,1 +1,29 @@
+const themeToggle = document.getElementById("themeToggle");
 
+const savedTheme = localStorage.getItem("theme");
+
+if(savedTheme === "dark"){
+    document.body.classList.add("dark-mode");
+
+    if(themeToggle){
+        themeToggle.textContent = "☀️";
+    }
+}
+
+if(themeToggle){
+
+    themeToggle.addEventListener("click", ()=>{
+
+        document.body.classList.toggle("dark-mode");
+
+        if(document.body.classList.contains("dark-mode")){
+            localStorage.setItem("theme", "dark");
+            themeToggle.textContent = "☀️";
+        }else{
+            localStorage.setItem("theme", "light");
+            themeToggle.textContent = "🌙";
+        }
+
+    });
+
+}
